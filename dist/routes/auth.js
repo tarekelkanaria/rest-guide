9b70d75197f41f9d6a00c7d1a40cca19779c392e
Tarek Elkanaria <tarekelkanaria@gmail.com>
2024-05-07 18:25:45 +0300

    Remove dist

--- dist/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_1 = require("../controllers/user");
-const validation_1 = require("../utils/validation");
-const router = (0, express_1.Router)();
-router.put("/signup", validation_1.signupValidation, user_1.signUp);
-router.post("/login", user_1.login);
-exports.default = router;
--- dist/utils/image-relative-path.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.imageRelativePath = void 0;
-const imageRelativePath = (imagePath) => {
-    return `images/${imagePath.substring(imagePath.lastIndexOf("\\") + 1)}`;
-};
-exports.imageRelativePath = imageRelativePath;
